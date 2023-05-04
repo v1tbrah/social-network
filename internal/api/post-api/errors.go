d12756754ff3a69e285b6b3ac03f8c76433e8ee3
Viktor <khatibvictor97@gmail.com>
2023-05-04 22:28:59 +0400

    fix: Remove all for review

--- internal/api/post-api/errors.go
+++
@@ -1,8 +0,0 @@
-package papi
-
-import "errors"
-
-var (
-	errInvalidID = errors.New("invalid id")
-	errEmptyName = errors.New("empty name")
-)
--- internal/send/err.go
+++
@@ -1,9 +0,0 @@
-package send
-
-type Error struct {
-	Msg string `json:"msg"`
-}
-
-func NewErr(msg string) Error {
-	return Error{Msg: msg}
-}
